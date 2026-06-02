95c609312428c202548fcaeeaa55222102a0614b
Anida Osmani <anidaosmani8@gmail.com>
2026-06-02 02:03:22 +0200

    Create books table and model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'author',
+        'genre',
+        'status',
+        'rating',
+        'review',
+        'user_id'
+    ];
 }
--- database/migrations/2026_06_01_235025_create_books_table.php
+++ database/migrations/2026_06_01_235025_create_books_table.php
@@ -1,25 +1,45 @@
-public function up(): void
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::create('books', function (Blueprint $table) {
-        $table->id();
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
 
-        $table->foreignId('user_id')
-              ->constrained()
-              ->onDelete('cascade');
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onDelete('cascade');
 
-        $table->string('title');
-        $table->string('author');
-        $table->string('genre');
+            $table->string('title');
+            $table->string('author');
+            $table->string('genre');
 
-        $table->enum('status', [
-            'Want To Read',
-            'Currently Reading',
-            'Finished'
-        ]);
+            $table->enum('status', [
+                'Want To Read',
+                'Currently Reading',
+                'Finished'
+            ]);
 
-        $table->integer('rating')->nullable();
-        $table->text('review')->nullable();
+            $table->integer('rating')->nullable();
+            $table->text('review')->nullable();
 
-        $table->timestamps();
-    });
-}
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
